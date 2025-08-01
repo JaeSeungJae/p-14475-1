f0f1bdea3ec0d151f4502bd285ec6340ad67325a
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-08-01 14:41:35 +0900

    step5 g,b

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,9 +2,24 @@ package com.back;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBit = expression.split("\\+");
-        return
-                Integer.parseInt(expressionBit[0].trim()) +
-                Integer.parseInt(expressionBit[1].trim());
+        String[] expressionBit = expression.split(" ");
+        String operator = " ";
+        for (String s : expressionBit) {
+            if (s.equals("+")) {
+                operator = "+";
+            }
+            if (s.equals("-")) {
+                operator = "-";
+            }
+        }
+        int num1 = Integer.parseInt(expressionBit[0]);
+        int num2 = Integer.parseInt(expressionBit[2]);
+        if (operator.equals("+")) {
+            return num1 + num2;
+        }
+        if (operator.equals("-")) {
+            return num1 - num2;
+        }
+        return 0;
     }
 }

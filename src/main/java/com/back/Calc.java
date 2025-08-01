9e0d44b5ad7f6c3420ad248bbab62a10053054c2
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-08-01 14:30:44 +0900

    step3 b

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -2,9 +2,9 @@ package com.back;
 
 public class Calc {
     public static int run(String expression) {
-        if (expression.equals("1 + 1")) return 2;
-        if (expression.equals("2 + 1")) return 3;
-        if (expression.equals("2 + 2")) return 4;
-        return 2;
+        String[] expressionBit = expression.split("\\+");
+        return
+                Integer.parseInt(expressionBit[0].trim()) +
+                Integer.parseInt(expressionBit[1].trim());
     }
 }

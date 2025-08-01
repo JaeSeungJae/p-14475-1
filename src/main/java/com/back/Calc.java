fbc10f9bb4ddeb1926ec24b0dff03590c20bc13c
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-08-01 14:28:09 +0900

    step3 g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -3,7 +3,8 @@ package com.back;
 public class Calc {
     public static int run(String expression) {
         if (expression.equals("1 + 1")) return 2;
-        else if (expression.equals("2 + 1")) return 3;
+        if (expression.equals("2 + 1")) return 3;
+        if (expression.equals("2 + 2")) return 4;
         return 2;
     }
 }

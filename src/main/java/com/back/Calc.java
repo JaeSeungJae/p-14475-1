752695e7b45a771e4b4dda6306faaae218164cbb
JAESEUNGJAE <valkyrie9923@gmail.com>
2025-08-04 19:22:05 +0900

    step18 g

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -1,17 +1,44 @@
 package com.back;
 
 import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBit = expression.split(" ");
-        int op_count = 0;
-        int num_count = 0;
-        //String[] operator = new String[expressionBit.length];
+        Pattern pattern = Pattern.compile("\\(([^)]+)\\)");
+        Matcher matcher = pattern.matcher(expression);
+        ArrayList<String> extractExpression = new ArrayList<>();
+        while (matcher.find()) {
+            extractExpression.add(matcher.group(1));
+        }
         ArrayList<String> operator = new ArrayList<>();
-        //int[] num = new int[expressionBit.length];
         ArrayList<Integer> num = new ArrayList<>();
+        for (String exp : extractExpression) {
+            ArrayList<String> operatorTemp = new ArrayList<>();
+            ArrayList<Integer> numTemp = new ArrayList<>();
+            manipulate(exp, numTemp, operatorTemp);
+            while (operatorTemp.stream().anyMatch(op -> op.equals("*"))) {
+                findMul(operatorTemp, numTemp);
+            }
+            num.add(operation(numTemp.size(), numTemp, operatorTemp, 0, 0));
+        }
+
+        String expression2 = expression.replaceAll("\\(([^)]+)\\)", "");
+
+
+        manipulate(expression2, num, operator);
+        while (operator.stream().anyMatch(op -> op.equals("*"))) {
+            findMul(operator, num);
+        }
+        return operation(num.size(), num, operator, 0, 0);
+    }
+
+    public static void manipulate(String expression, ArrayList<Integer> num, ArrayList<String> operator) {
+        String[] expressionBit = expression.split(" ");
         for (String s : expressionBit) {
+            if (s.isEmpty()) {
+                continue;
+            }
             if (s.equals("+")) {
                 operator.add("+");
             } else if (s.equals("-")) {
@@ -22,10 +49,6 @@ public class Calc {
                 num.add(Integer.parseInt(s));
             }
         }
-        while (operator.stream().anyMatch(op -> op.equals("*"))) {
-            findMul(operator, num);
-        }
-        return operation(num.size(), num, operator, 0, 0);
     }
 
     public static int operation(int num_count, ArrayList<Integer> num, ArrayList<String> operator, int count, int sum) {
